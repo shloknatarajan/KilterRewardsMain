065ce38ac73c9b7a6d0a4b98a631d57e5d1b4e79
Shlok Natarajan <shloknatarajan@users.noreply.github.com>
2017-11-26 12:59:35 -0600

    Lightened and made testing

--- js/main.js
+++ js/main.js
@@ -22,6 +22,13 @@ window.addEventListener('resize', function() {
     over.style.visibility = "hidden";
 });
 
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+
 
   
   

5a0dcd8a1cde9fe9e7a0a5b8ce06a7f272b374b7
Shlok Natarajan <snatarajan542@rsdmo.org>
2018-06-19 13:54:35 -0500

    Colton Henning Welcome Page
    finished the welcome page of cole's personal site

--- js/main.js
+++ js/main.js
@@ -1,37 +1,11 @@
-var preloader = document.getElementById("preloader");
-window.addEventListener('load', function() {
-    setTimeout(function() {
-        preloader.style.display = "none";        
-    }, (1000));
-});
-
-var count = 0;
-var over = document.getElementById("overlay");    
-function expandoverlay() {
-    count++;
-    if (count % 2 == 0) {
-        over.style.visibility = "hidden";        
-        over.style.opacity = 0;
-    } else {
-        over.style.visibility = "visible";         
-        over.style.opacity = 1;
-    }
-}
-
-window.addEventListener('resize', function() {
-    over.style.visibility = "hidden";
-});
-
-$(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+$(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 500) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
     });
-  });
-
-
-  
-  
-
-
-  
+});
